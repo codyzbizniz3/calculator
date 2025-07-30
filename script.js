430117eaeaa39aea95e0da32434f8e54d3477ddd
codyzbizniz3 <codyzbizniz3@users.noreply.github.com>
2025-07-30 10:28:25 -0700

    Move numberButtons logic to numberClick function for consistency

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const inputField = document.getElementById('input-field');
 const numberButtons = document.querySelectorAll('.number-btn');
 const clearButton = document.getElementById('clear-btn');
+const operatorButtons = document.querySelectorAll('.operator-btn')
 
 let currentInput = '0';
 let awaitingNewNumber = false;
@@ -14,19 +15,24 @@ function clearField() {
     updateDisplay();
 }
 
+function numberClick() {
+    const digit = this.textContent;
+
+    if (currentInput === '0' || awaitingNewNumber) {
+        currentInput = digit;
+        awaitingNewNumber = false;
+    } else {
+        currentInput += digit;
+    }
+    updateDisplay()
+}
+
+function currentOperator() {
+
+}
+
 numberButtons.forEach(button => {
-    button.addEventListener("click", function () {
-        const digit = this.textContent;
-
-        if (currentInput === '0' || awaitingNewNumber) {
-            currentInput = digit;
-            awaitingNewNumber = false;
-        } else {
-            currentInput += digit;
-        }
-        updateDisplay()
-    });
-
+    button.addEventListener("click", numberClick)
 });
 
 clearButton.addEventListener('click', clearField);
